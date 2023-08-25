b8ec0471aa43f84776955607c52672de85bc5f95
hoduy511 <hoduy244@gmail.com>
2023-08-25 11:44:27 +0700

    fix feature send mail to verify

--- Stage3/xulydangky.php
+++ Stage3/xulydangky.php
@@ -1,13 +1,22 @@
 <?php
 include "connectmysql.php";
 include "thanhvien.php";
-$mssv = $_POST["mssv"];
-$hoten = $_POST["hoten"];
-$email = $_POST["email"];
-$gioitinh = $_POST["gioitinh"];
-$sothich = implode(", ", $_POST["sothich"]);
-$quoctich = $_POST["quoctich"];
-$ghichu = $_POST["ghichu"];
+$mssv = null;
+$hoten = null;
+$email = null;
+$gioitinh = null;
+$sothich = null;
+$quoctich = null;
+$ghichu = null;
+
+if (isset($_POST["mssv"])) $mssv = $_POST["mssv"];
+if (isset($_POST["hoten"])) $hoten = $_POST["hoten"];
+if (isset($_POST["email"])) $email = $_POST["email"];
+if (isset($_POST["gioitinh"])) $gioitinh = $_POST["gioitinh"];
+if (isset($_POST["sothich"])) $sothich = implode(', ',$_POST["sothich"]);
+if (isset($_POST["quoctich"])) $quoctich = $_POST["quoctich"];
+if (isset($_POST["ghichu"])) $ghichu = $_POST["ghichu"];
+
 settype($gioitinh, "int");
 // validate
 $mssv = trim(strip_tags($mssv));
@@ -41,7 +50,7 @@ if ($loi != "") { ?>
             </div>
         </div>
     </div>
-<?php } else {
+    <?php } else {
     // authentication
     $thanhvien = new thanhvien($_POST["mssv"], $_POST["hoten"], $_POST["email"], $_POST["gioitinh"], implode(", ", $_POST["sothich"]), $_POST["quoctich"], $_POST["ghichu"]);
     try {
@@ -49,56 +58,68 @@ if ($loi != "") { ?>
             echo "Bạn đã đăng ký trước đó rồi";
         } else
             if ($thanhvien->is_exist_email()) {
-                echo "Email đã được sử dụng";
-            } else {
-                $code = random_int(1000, 9999);
-                // send code to email
-                require("PHPMailer-master/src/PHPMailer.php");
-                require("PHPMailer-master/src/Exception.php");
-                require("PHPMailer-master/src/OAuth.php");
-                require("PHPMailer-master/src/SMTP.php");
-                $mail = new PHPMailer\PHPMailer\PHPMailer(true);
-                try {
-                    $mail->SMTPDebug = 2; //chế độ full debug, khi mọi thứ ok thì chỉnh lại 0
-                    $mail->isSMTP(); // Set mailer to use SMTP
-                    $mail->Host = 'smtp.gmail.com'; // Server gửi thư
-                    $mail->SMTPAuth = true;
-                    $mail->Username = 'cuoncamauit@gmail.com'; // ví dụ: abc@gmail.com
-                    $mail->Password = 'rhbqvattwsijrmzq';
-                    $mail->SMTPSecure = 'ssl'; //TLS hoặc `ssl` 
-                    $mail->Port = 465; // 587 hoặc 465
-                    $mail->CharSet = "UTF-8";
-                    $mail->smtpConnect(
-                        [
-                            "ssl" => [
-                                "verify_peer" => false,
-                                "verify_peer_name" => false,
-                                "allow_self_signed" => true
-                            ]
-                        ]
-                    );
-                    //Khai báo người gửi và người nhận mail        
-                    $mail->setFrom($email, 'Ban quản trị website');
-                    $mail->addAddress("{$email}", 'Quý khách');
-                    $mail->isHTML(true); // nội dung của email có mã HTML
-                    $mail->Subject = 'Cấp lại mật khẩu mới';
-                    $mail->Body = "Đây là mã xác thực của bạn <b> {$code} </b>";
-                    $mail->send();
-                    $thongbao .= "Đã gửi mail thành công<br>";
-                } catch (Exception $e) {
-                    $thongbao .= "Lỗi khi gửi thư: " . $mail->ErrorInfo;
-                }
+            echo "Email đã được sử dụng";
+        } else {
+            $code = random_int(1000, 9999);
+            // send code to email
+            require("PHPMailer-master/src/PHPMailer.php");
+            require("PHPMailer-master/src/SMTP.php");
+            require("PHPMailer-master/src/Exception.php");
 
+            //Create an instance; passing `true` enables exceptions
+            $mail = new PHPMailer\PHPMailer\PHPMailer();
 
-                ?>
-                <h2>XÁC THỰC EMAIL</h2>
-                <label for="">Nhập mã xác thực</label>
-                <input type="text" name="" id="code">
-                <button>Kiểm tra</button>
-                <?php
-                //$thanhvien->insert();
-                echo "Đăng ký thành công";
+            try {
+                //Server settings
+                $mail->SMTPDebug = 0;                      //Enable verbose debug output
+                $mail->isSMTP();
+                $mail->SMTPSecure = 'ssl';                                   //Send using SMTP
+                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                $mail->Username   = 'cuongcamauit@gmail.com';                     //SMTP username
+                $mail->Password   = 'vpdfwungmoqnoehr';                               //SMTP password
+                //Enable implicit TLS encryption
+                $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+                //Recipients
+                $mail->setFrom('cuongcamauit@gmail.com', 'Mailer');
+                $mail->addAddress($email, 'Joe User');     //Add a recipient
+                //$mail->addAddress('ellen@example.com');               //Name is optional
+                // $mail->addReplyTo('2051120209@ut.edu.vn', 'Information');
+                // $mail->addCC('cc@example.com');
+                // $mail->addBCC('bcc@example.com');
+
+                //Attachments
+                // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+                // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+                //Content
+                $mail->isHTML(true);                                  //Set email format to HTML
+                $mail->Subject = 'Verify email';
+                $mail->Body    = 'Your verify code: ' . "<b>$code</b>";
+                //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                $mail->send();
+                echo 'Message has been sent';
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
             }
+
+    ?>
+            <script>
+
+            </script>
+            <h2>XÁC THỰC EMAIL</h2>
+            <label for="">Nhập mã xác thực</label>
+            <input type="text" name="" id="code">
+            <button onclick="javascript:(function () {
+                code = document.getElementById('code').value; if (code==<?php print($code) ?>) { document.getElementById('thongbao').innerHTML = 'Đăng Ký thành công'; <?php $thanhvien->insert(); ?>} else document.getElementById('thongbao').innerHTML = 'Vui lòng nhập lại mã xác thực';})()">Kiểm tra</button>
+            <div id="thongbao">
+
+            </div>
+<?php
+            
+        }
     } catch (\Throwable $th) {
         throw $th;
     }
